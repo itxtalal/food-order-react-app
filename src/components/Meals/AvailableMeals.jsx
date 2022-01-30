dba3bb10e2216c9b327073d0b5e5321200ecf239
Talal <talalstudent@gmail.com>
2022-01-30 18:54:16 +0500

    Fetching Meals from Firebase

--- src/components/Meals/AvailableMeals.jsx
+++ src/components/Meals/AvailableMeals.jsx
@@ -1,12 +1,66 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Card from "../UI/Card";
 import MealItem from "./MealItem/MealItem";
 
 import classes from "./AvailableMeals.module.css";
-import DUMMY_MEALS from "./dummy-meals";
 
 const AvailableMeals = () => {
-  const mealsList = DUMMY_MEALS.map((meal) => {
+  const [meals, setMeals] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [httpError, setHttpError] = useState();
+
+  useEffect(() => {
+    const fetchMeals = async () => {
+      const response = await fetch(
+        "https://http-req-62505-default-rtdb.firebaseio.com/meals.json"
+      );
+      // It gives back an object response
+      // Need to convert to Array
+
+      if (!response.ok) {
+        throw new Error("Failed to Fetch!");
+      }
+      const responseData = await response.json();
+
+      const loadedMeals = [];
+
+      for (const key in responseData) {
+        loadedMeals.push({
+          id: key,
+          ...responseData[key],
+        });
+      }
+      setMeals(loadedMeals);
+      setIsLoading(false);
+    };
+
+    // We cant use try catch to catch error
+    // Throwing a new Error will reject the promise
+    // But to handle the Error we need to use .catch on the sync function
+    // making it async/await will loop the useEffect
+    fetchMeals().catch((error) => {
+      setIsLoading(false);
+      setHttpError(error.message);
+    });
+  }, []);
+
+  if (isLoading) {
+    return (
+      <section className={classes.MealsLoading}>
+        <p>Loading...</p>
+      </section>
+    );
+  }
+
+  if (httpError) {
+    return (
+      <section className={classes.MealsError}>
+        <p>{httpError}</p>
+      </section>
+    );
+  }
+
+  const mealsList = meals.map((meal) => {
     return (
       <MealItem
         id={meal.id}
---
+++ src/hooks/use-http.jsx
@@ -0,0 +1,40 @@
+import { useState, useCallback } from "react";
+
+const useHttp = (applyData) => {
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const sendRequest = useCallback(
+    async (requestConfig) => {
+      setIsLoading(true);
+      setError(null);
+      try {
+        const response = await fetch(requestConfig.url, {
+          method: requestConfig.method ? requestConfig.method : "GET",
+          headers: requestConfig.headers ? requestConfig.headers : {},
+          body: requestConfig.body ? JSON.stringify(requestConfig.body) : null,
+        });
+
+        if (!response.ok) {
+          throw new Error("Request failed!");
+        }
+
+        const data = await response.json();
+
+        applyData(data);
+      } catch (err) {
+        setError(err.message || "Something went wrong!");
+      }
+      setIsLoading(false);
+    },
+    [applyData]
+  );
+
+  return {
+    isLoading,
+    error,
+    sendRequest,
+  };
+};
+
+export default useHttp;
